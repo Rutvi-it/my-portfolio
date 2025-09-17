8d43c822dec86ba4096c2e22ebd65b7b1598e285
Rutvi-it <jayjhdp@gmail.com>
2025-09-17 17:35:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
  
-// Dark Mode Toggle
-const toggleBtn = document.getElementById("toggleMode");
-toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-});
-
-// Mobile Menu Toggle
-const menuToggle = document.getElementById("menuToggle");
-const navMenu = document.querySelector("nav ul");
-
-menuToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("show");
-});
