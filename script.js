fd402ded4a3884ebd8d8789624daa4fd0f2986d4
Rutvi-it <jayjhdp@gmail.com>
2025-09-17 17:49:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,18 +2,25 @@
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
 
-  // save theme in local storage
+  const btn = document.getElementById("theme-btn");
+
   if (document.body.classList.contains("dark-mode")) {
     localStorage.setItem("theme", "dark");
+    btn.textContent = "☀️"; // change icon
   } else {
     localStorage.setItem("theme", "light");
+    btn.textContent = "🌙"; // change icon
   }
 }
 
 // Load theme on page load
 window.onload = function () {
+  const btn = document.getElementById("theme-btn");
   if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark-mode");
+    btn.textContent = "☀️";
+  } else {
+    btn.textContent = "🌙";
   }
 };
 
